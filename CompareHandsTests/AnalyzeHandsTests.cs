20ccc9320a349989e43532732bc65e1d4b76d111
BuyInClub <61022118+BuyInClub@users.noreply.github.com>
2020-10-29 22:11:27 -0400

    Add files via upload

---
+++ CompareHandsTests/AnalyzeHandsTests.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompareHands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareHands.Tests
+{
+    [TestClass()]
+    public class AnalyzeHandsTests
+    {
+        [TestMethod()]
+        public void IsStraightAceHigh()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("10", Suit.Club),
+                    new Card("J", Suit.Club),
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Spade),
+                    new Card("Q", Suit.Diamond), 
+                    new Card("K", Suit.Club),
+                    new Card("A", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+            // Ace high straight
+            long finalRank = HandInfo.CalculateRank(HandRanking.Straight, new List<int> { 14 }, new List<int> { });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsStraightFiveHigh()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("10", Suit.Club),
+                    new Card("J", Suit.Club),
+                    new Card("2", Suit.Club),
+                    new Card("5", Suit.Spade),
+                    new Card("3", Suit.Diamond),
+                    new Card("4", Suit.Club),
+                    new Card("A", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // Ace high straight
+            long finalRank = HandInfo.CalculateRank(HandRanking.Straight, new List<int> { 5 }, new List<int> { });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+        [TestMethod()]
+        public void IsStraightFlush()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("2", Suit.Heart),
+                    new Card("J", Suit.Diamond),
+                    new Card("2", Suit.Club),
+                    new Card("5", Suit.Club),
+                    new Card("3", Suit.Club),
+                    new Card("4", Suit.Club),
+                    new Card("A", Suit.Club)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // Ace high straight flush
+            long finalRank = HandInfo.CalculateRank(HandRanking.StraightFlush, new List<int> { 5 }, new List<int> { });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+        [TestMethod()]
+        public void IsRoyalStraightFlush()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("J", Suit.Diamond),
+                    new Card("2", Suit.Club),
+                    new Card("10", Suit.Diamond),
+                    new Card("Q", Suit.Diamond),
+                    new Card("4", Suit.Club),
+                    new Card("A", Suit.Diamond)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // royal straight flush
+            long finalRank = HandInfo.CalculateRank(HandRanking.RoyalStraightFlush, new List<int> { 14 }, new List<int> { });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsFourOfAKind()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("J", Suit.Diamond),
+                    new Card("K", Suit.Club),
+                    new Card("K", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    new Card("4", Suit.Club),
+                    new Card("K", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // four of a kind
+            long finalRank = HandInfo.CalculateRank(HandRanking.FourOfAKind, new List<int> { 13 }, new List<int> { 12 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsFullHouse()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("J", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("K", Suit.Spade),
+                    new Card("J", Suit.Heart),
+                    new Card("4", Suit.Club),
+                    new Card("K", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // full house
+            long finalRank = HandInfo.CalculateRank(HandRanking.FullHouse, new List<int> { 13 }, new List<int> { 11 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsFlush()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("J", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("9", Suit.Diamond),
+                    new Card("8", Suit.Diamond),
+                    new Card("4", Suit.Diamond),
+                    new Card("K", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // flush
+            long finalRank = HandInfo.CalculateRank(HandRanking.Flush, new List<int> { 13, 11, 9, 8, 4 }, new List<int> { });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsThreeOfAKind()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("J", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("8", Suit.Diamond),
+                    new Card("4", Suit.Diamond),
+                    new Card("10", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // three of a kind
+            long finalRank = HandInfo.CalculateRank(HandRanking.ThreeOfAKind, new List<int> { 11 }, new List<int> { 13, 10 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+        [TestMethod()]
+        public void IsTwoPair()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("2", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("K", Suit.Spade),
+                    new Card("J", Suit.Diamond),
+                    new Card("4", Suit.Diamond),
+                    new Card("10", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // Two of a kind
+            long finalRank = HandInfo.CalculateRank(HandRanking.TwoPair, new List<int> { 13, 11 }, new List<int> { 10 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsPair()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("2", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("K", Suit.Spade),
+                    new Card("8", Suit.Diamond),
+                    new Card("4", Suit.Diamond),
+                    new Card("10", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // Pair
+            long finalRank = HandInfo.CalculateRank(HandRanking.Pair, new List<int> { 13 }, new List<int> { 11, 10, 8 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+        [TestMethod()]
+        public void IsHighCard()
+        {
+            AnalyzeHands analyzeHands = new AnalyzeHands();
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    new Card("K", Suit.Diamond),
+                    new Card("2", Suit.Diamond),
+                    new Card("J", Suit.Club),
+                    new Card("3", Suit.Spade),
+                    new Card("8", Suit.Diamond),
+                    new Card("4", Suit.Diamond),
+                    new Card("10", Suit.Heart)
+                } },
+            };
+
+            List<HandInfo> results = analyzeHands.OrderHands(hands);
+
+            // High card
+            long finalRank = HandInfo.CalculateRank(HandRanking.HighCard, new List<int> { }, new List<int> { 13, 11, 10, 8, 4 });
+            Assert.AreEqual(results[0].handRank, finalRank);
+        }
+
+    }
+}
---
+++ CompareHandsTests/BadBeatTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using CompareHands;
+
+namespace CompareHandsTests
+{
+    [TestClass]
+    public class BadBeatTests
+    {
+        [TestMethod]
+        public void BadBeatOtherPlayerHasOut()
+        {
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    // board
+                    new Card("8", Suit.Spade),
+                    new Card("5", Suit.Spade),
+                    new Card("8", Suit.Diamond),
+                    new Card("3", Suit.Club),
+                    // in hand
+                    new Card("10", Suit.Spade),
+                    new Card("6", Suit.Spade)
+                } },
+                { "Chris", new List<Card> {
+                    // board
+                    new Card("8", Suit.Spade),
+                    new Card("5", Suit.Spade),
+                    new Card("8", Suit.Diamond),
+                    new Card("3", Suit.Club),
+                    // in hand
+                    new Card("8", Suit.Club),
+                    new Card("6", Suit.Diamond)
+                } },
+            };
+
+            BadBeatCalc badBeatCalc = new BadBeatCalc();
+            String sixCardWinner;
+            List<Card> numOuts = badBeatCalc.DetermineBadBeat("Dave", hands, out sixCardWinner);
+
+            Assert.AreEqual(numOuts.Count, 8);
+            Assert.AreEqual(sixCardWinner, "Chris");
+        }
+
+
+        [TestMethod]
+        public void BadBeatTiedOnTurn()
+        {
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                { "Dave", new List<Card> {
+                    // board
+                    new Card("2", Suit.Spade),
+                    new Card("2", Suit.Club),
+                    new Card("K", Suit.Heart),
+                    new Card("K", Suit.Diamond),
+                    // in hand
+                    new Card("6", Suit.Club),
+                    new Card("K", Suit.Spade)
+                } },
+                { "Chris", new List<Card> {
+                    // board
+                    new Card("2", Suit.Spade),
+                    new Card("2", Suit.Club),
+                    new Card("K", Suit.Heart),
+                    new Card("K", Suit.Diamond),
+                    // in hand
+                    new Card("K", Suit.Club),
+                    new Card("J", Suit.Diamond)
+                } },
+            };
+
+            BadBeatCalc badBeatCalc = new BadBeatCalc();
+            String sixCardWinner;
+            List<Card> numOuts = badBeatCalc.DetermineBadBeat("Chris", hands, out sixCardWinner);
+
+            Assert.AreEqual(numOuts.Count, 3);
+            Assert.AreEqual(sixCardWinner, "Dave");
+        }
+
+
+        [TestMethod]
+        public void BadBeatTwoPlayers()
+        {
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                // Dave has a set of 6's, Chris an opened end straight
+                // Chris had 4 outs for a 10 to hit the straight
+                { "Dave", new List<Card> {
+                    // board
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    // in hand
+                    new Card("6", Suit.Diamond),
+                    new Card("6", Suit.Heart)
+                } },
+                { "Chris", new List<Card> {
+                    // board
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    // in hand
+                    new Card("K", Suit.Club),
+                    new Card("A", Suit.Heart)
+                } },
+            };
+
+            BadBeatCalc badBeatCalc = new BadBeatCalc();
+            String sixCardWinner;
+            List<Card> numOuts = badBeatCalc.DetermineBadBeat("Chris", hands, out sixCardWinner);
+
+            Assert.AreEqual(numOuts.Count, 4);
+        }
+
+        [TestMethod]
+        public void BadBeatThreePlayers()
+        {
+            Dictionary<String, List<Card>> hands = new Dictionary<String, List<Card>>
+            {
+                // Dave has a set of 6's, Chris an opened end straight
+                // Chris had 4 outs for a 10 to hit the straight
+                { "Dave", new List<Card> {
+                    // board
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    // in hand
+                    new Card("6", Suit.Diamond),
+                    new Card("6", Suit.Heart)
+                } },
+                { "Chris", new List<Card> {
+                    // board
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    // in hand
+                    new Card("K", Suit.Club),
+                    new Card("A", Suit.Heart)
+                } },
+                { "Jim", new List<Card> {
+                    // board
+                    new Card("5", Suit.Club),
+                    new Card("6", Suit.Club),
+                    new Card("J", Suit.Spade),
+                    new Card("Q", Suit.Diamond),
+                    // in hand
+                    new Card("Q", Suit.Club),
+                    new Card("Q", Suit.Heart)
+                } },
+            };
+
+            BadBeatCalc badBeatCalc = new BadBeatCalc();
+            String sixCardWinner;
+            List<Card> numOuts = badBeatCalc.DetermineBadBeat("Chris", hands, out sixCardWinner);
+
+            Assert.AreEqual(numOuts.Count, 4);
+            Assert.AreEqual(sixCardWinner, "Jim");
+        }
+
+    }
+}
